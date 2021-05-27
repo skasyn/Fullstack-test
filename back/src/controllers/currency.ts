57c1e95d3610308648a36e3ee3b1a5fcddc1912a
Antoine FP <antoine.folieplaineau@gmail.com>
2021-05-28 01:46:30 +0200

    add: parameters in query

--- back/src/controllers/currency.ts
+++ back/src/controllers/currency.ts
@@ -8,8 +8,18 @@ export const getAllCurrency = async (req: Request, res: Response) => {
   res.status(200).json(currencies);
 }
 
-export const getConversionRate = async (req: Request, res: Response) => {
-  const conversionRate = getFromCache("USD_GBP") ?? addToCache("USD_GBP", (await getConversion("USD_GBP")).data);
+interface GetConversionRateInterface {
+  conversion: string;
+}
+
+export const getConversionRate = async (req: Request<unknown, unknown, unknown, GetConversionRateInterface>, res: Response) => {
+  const { query } = req;
+
+  if (!query.conversion) {
+    res.status(400).send({ message: "Missing conversion"});
+    return;
+  }
+  const conversionRate = getFromCache(query.conversion) ?? addToCache(query.conversion, (await getConversion(query.conversion)).data);
 
   res.status(200).json(conversionRate);
 }
